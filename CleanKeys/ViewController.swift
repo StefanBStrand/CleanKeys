b3223f169a58aaedbb8c03dbdaf8baf0546bdcc1
Stefan Strand <stefanboytchev@hotmail.com>
2024-05-21 22:16:43 +0200

    Initial Commit

---
+++ CleanKeys/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CleanKeys
+//
+//  Created by Stefan Boytchev on 21/05/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
