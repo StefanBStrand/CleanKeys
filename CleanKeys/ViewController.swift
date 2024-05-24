d7a1d01f23aa91ae41cf09b26bd4ea34153f6e7e
Stefan Strand <stefanboytchev@hotmail.com>
2024-05-24 23:03:09 +0200

    Still working on body, disableKeyboard function

--- CleanKeys/ViewController.swift
+++ CleanKeys/ViewController.swift
@@ -24,6 +24,37 @@ class ViewController: NSViewController {
     @IBAction func disableInput(_ sender: NSButtonCell) {
         
         let alert = NSAlert()
+        alert.messageText = "Disable Input"
+        alert.informativeText = "This will disable your keyboard for 5 seconds. Do you want to proceed?"
+        alert.alertStyle = .warning
+        alert.addButton(withTitle: "Disable")
+        alert.addButton(withTitle: "Cancel")
+        
+        let response = alert.runModal()
+        if response == .alertFirstButtonReturn {
+            disableKeyboard()
+        }
+    }
+    
+    func disableKeyboard() {
+        manager = IOHIDManagerCreate(kCFAllocatorDefault, IOOptionBits(kIOHIDOptionsTypeNone))
+        IOHIDManagerSetDeviceMatching(manager, nil)
+        
+        if IOHIDManagerOpen(manager!, IOOptionBits(kIOHIDOptionsTypeNone)) != kIOReturnSuccess {
+            showAlert("Failed to open HID manager")
+            return
+        }
+        
+        let devices = IOHIDManagerCopyDevices(manager!) as? Set<IOHIDDevice>
+        keyboardDevices = devices?.filter { device in
+            guard let properties = IOHIDDeviceGetProperty(device, kIOHIDPrimaryUsageKey as CFString) else {
+                return false
+                
+            }
+            
+            let usagePage = properties as! Int
+            return usagePage == kHIDPage_GenericDesktop && (IOHIDDeviceGetProperty(device, kIOHIDPrimaryUsageKey as CFString) as! Int == kHIDUsage_GD_Keyboard)
+        }
         
     }
     
