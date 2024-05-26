a810739fd27ceccc12f9467cca821996547b117d
Stefan Strand <stefanboytchev@hotmail.com>
2024-05-26 22:36:57 +0200

    First version of program code complete?

--- CleanKeys/ViewController.swift
+++ CleanKeys/ViewController.swift
@@ -11,7 +11,7 @@ import IOKit.hid
 class ViewController: NSViewController {
     
     var manager: IOHIDManager?
-    var devices: Set<IOHIDDevice>?
+    var keyboardDevices: Set<IOHIDDevice>?
     var disableTimer: Timer?
     
     override func viewDidLoad() {
@@ -37,29 +37,64 @@ class ViewController: NSViewController {
     }
     
     func disableKeyboard() {
-        manager = IOHIDManagerCreate(kCFAllocatorDefault, IOOptionBits(kIOHIDOptionsTypeNone))
+        // Create the HID Manager
+        let manager = IOHIDManagerCreate(kCFAllocatorDefault, IOOptionBits(kIOHIDOptionsTypeNone))
+        
+        // Set device matching to nil to get all devices
         IOHIDManagerSetDeviceMatching(manager, nil)
         
-        if IOHIDManagerOpen(manager!, IOOptionBits(kIOHIDOptionsTypeNone)) != kIOReturnSuccess {
-            showAlert("Failed to open HID manager")
+        // Open the HID Manager
+        if IOHIDManagerOpen(manager, IOOptionBits(kIOHIDOptionsTypeNone)) != kIOReturnSuccess {
+            showAlert("Failed to open HID Manager")
             return
         }
         
-        let devices = IOHIDManagerCopyDevices(manager!) as? Set<IOHIDDevice>
-        keyboardDevices = devices?.filter { device in
-            guard let properties = IOHIDDeviceGetProperty(device, kIOHIDPrimaryUsageKey as CFString) else {
+        // Get the set of all HID devices
+        guard let devices = IOHIDManagerCopyDevices(manager) as? Set<IOHIDDevice> else {
+            showAlert("Failed to get HID devices")
+            return
+        }
+        
+        // Filter the devices to get only keyboards
+        keyboardDevices = devices.filter { device in
+            guard let properties = IOHIDDeviceGetProperty(device, kIOHIDPrimaryUsagePageKey as CFString) else {
                 return false
-                
             }
-            
             let usagePage = properties as! Int
             return usagePage == kHIDPage_GenericDesktop && (IOHIDDeviceGetProperty(device, kIOHIDPrimaryUsageKey as CFString) as! Int == kHIDUsage_GD_Keyboard)
         }
         
+        // Close (disable) each keyboard device
+        keyboardDevices?.forEach { device in
+            if IOHIDDeviceClose(device, IOOptionBits(kIOHIDOptionsTypeNone)) != kIOReturnSuccess {
+                showAlert("Failed to disable keyboard")
+            }
+        }
+        
+        // Use a shorter timer for initial testing
+        disableTimer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(enableKeyboard), userInfo: nil, repeats: false)
+    }
+
+    
+    @objc func enableKeyboard() {
+        keyboardDevices?.forEach { device in
+            if IOHIDDeviceOpen(device, IOOptionBits(kIOHIDOptionsTypeNone)) != kIOReturnSuccess {
+                showAlert("Failed to re-enable keyboard")
+            }}
+        disableTimer?.invalidate()
     }
     
     
-
+    func showAlert(_ message: String) {
+        let alert = NSAlert()
+        alert.messageText = "Error"
+        alert.informativeText = message
+        alert.alertStyle = .critical
+        alert.addButton(withTitle: "OK")
+        alert.runModal()
+    }
+    
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
